257d1cd9dea049aeba2887be47fd8859de71b62a
nurlanemir <nurlan.emiroff@gmail.com>
2017-10-01 13:54:11 +0200

    fixes merge conflicts

--- app/models/laundry_room.rb
+++ app/models/laundry_room.rb
@@ -5,13 +5,8 @@ class LaundryRoom < ApplicationRecord
   before_validation :add_schedule
 
   def add_schedule
-<<<<<<< HEAD
-    self.schedule = IceCube::Schedule.new(Time.now.in_time_zone(Time.zone))
-    self.schedule.add_recurrence_rule IceCube::Rule.daily.hour_of_day(8, 13)
-=======
     self.schedule = IceCube::Schedule.new(Time.now.in_time_zone(Time.zone).beginning_of_day)
     self.schedule.add_recurrence_rule IceCube::Rule.daily.hour_of_day(8,13)
->>>>>>> a1636826a59202b9c7d3ac5613bdca98fd476742
   end
 
 
@@ -19,4 +14,3 @@ class LaundryRoom < ApplicationRecord
     self.schedule.next_occurrence
   end
 end
-
