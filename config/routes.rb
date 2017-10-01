3272eacec6174cb10930fab4c7bc1f4a94b322bc
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-01 17:19:42 +0200

    first attempt, added unbook link

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root controller: :laundry_rooms, action: :index
   resources :laundry_rooms, only: [:index] do
-    get :create_booking
+    get :create_booking, :delete_booking
   end
   devise_for :users
 
